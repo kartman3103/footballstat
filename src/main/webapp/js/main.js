24a7fd58f371fc94b1d55c596a3704d784a9403c
kartman3103 <pasha3103@bk.ru>
2018-03-19 19:00:19 +0300

    check commit

--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -14,7 +14,6 @@ export const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ &&
     window.__REDUX_DEVTOOLS_EXTENSION__());
 
 loadAvailableLeagues();
-
 initLocalize();
 initLocalizeData();
 
